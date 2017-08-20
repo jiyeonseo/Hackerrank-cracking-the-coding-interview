25fe78c18c48f906c1b6e5725a494278a18e4a8a
jiyeonseo <seojeee@gmail.com>
2017-08-20 17:28:01 +0900

    implements “fibonacci” with memoization and dynamic programming approach

--- src/com/jiyeon/FibonacciNumbers.java
+++ src/com/jiyeon/FibonacciNumbers.java
@@ -1,5 +1,8 @@
 package com.jiyeon;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jiyeon on 2017. 7. 16..
  * [TECHNIQUES/CONCEPTS] Recursion
@@ -7,8 +10,35 @@ package com.jiyeon;
  */
 public class FibonacciNumbers {
 
-    // TODO : tail recusion으로 해봐야지ㅋ
+    public static int fibonacciDP(int n) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
 
+        int[] val = new int[n+1];
+        val[0] = 0;
+        val[1] = 1;
+        val[2] = 1;
+        for(int i = 3; i <= n ; i++) {
+            val[n] = fibonacciDP(n-1) + fibonacciDP(n-2);
+        }
+
+        return val[n];
+    }
+
+    public static int fibonacciMemo(int n) {
+        return fibonacciMemo(n, new int[n+1]);
+    }
+
+    public static int fibonacciMemo(int n, int[] memo) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+
+        if(memo[n] == 0) {
+            memo[n] = fibonacciMemo(n-1, memo) + fibonacciMemo(n-2, memo);
+        }
+
+        return memo[n]; 
+    }
     public static int fibonacci(int n) {
         // Complete the function.
         if(n==0) return 0;
@@ -19,9 +49,13 @@ public class FibonacciNumbers {
 
 
     public static void main(String[] args) {
-        int input = 3;
+        int input = 6;
 
-        System.out.println(fibonacci(input)); // 2
+        System.out.println(fibonacci(input)); // 8
+        System.out.println(fibonacciMemo(input)); // 8
+        System.out.println(fibonacciDP(input)); // 8
+//        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+//        System.out.println(fibonacciWithMemo2(3)); // 2
 
     }
 }
